c587d3ab527ad2d503f45038f67f8bd9ed9f1e1f
Tyler Reckart <tyler.reckart@gmail.com>
2015-09-06 17:41:36 -0400

    Reduce branch

--- bower_components/grunt-concurrent/test/fixtures/server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var net = require('net');
-
-var server = net.createServer(function (socket) {
-	socket.write('Hello world').pipe(socket);
-});
-
-server.listen(0, '127.0.0.1');
--- tests/seedfile.js
+++
@@ -1,5 +0,0 @@
-var seed = require('seed');
-
-seed.task('default', function() {
-  // place code for your default task here
-});
