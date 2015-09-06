4b696284da1da62c0a7e8f63e8893bc07dfb88fc
Tyler Reckart <tyler.reckart@gmail.com>
2015-09-06 19:25:54 -0400

    Update structure

--- gruntfile.js
+++ gruntfile.js
@@ -1,41 +1,50 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('bower.json'),
+		pkg: grunt.file.readJSON('package.json'),
+
 		sass: {
-			dist: {
-				files: {
-					'assets/css/main.css' : 'assets/scss/*.scss'
-				}
-			}
-		},
+      dist: {
+        files: {
+          'assets/css/main.css' : 'assets/scss/*.scss'
+        }
+      }
+    },
     less: {
-			dist: {
-				files: {
-					'assets/css/main.css' : 'assets/less/*.less'
-				}
-			}
-		},
-		watch: {
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			},
+      dist: {
+        files: {
+          'assets/css/main.css' : 'assets/less/*.less'
+        }
+      },
+      production: {
+        options: {
+          compress: true
+        }
+      }
+    },
+    watch : {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+          livereloadOnError: true
+        }
+      },
       sass: {
         files: '**/*.less',
-        tasks: ['less']
-      }
-		},
-    concurrent: {
-      options: {
-        logConcurrentOutput: true,
-      },
-      prod: {
-        tasks: ["watch:css", "watch:sass"]
+        tasks: ['less'],
+        options: {
+          livereload: true,
+          livereloadOnError: true
+        }
       }
     }
-	});
-  grunt.loadTasks('grunt-contrib-less');
-	grunt.loadTasks('grunt-contrib-sass');
-	grunt.loadTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+  });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default',['watch']);
+
 };
